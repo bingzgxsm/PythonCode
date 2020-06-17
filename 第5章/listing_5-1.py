2f11a78c3f3533719047c4498038323296c1f577
bingzgxsm <admin@gxsmjx.com>
2020-06-17 22:09:57 +0800

    从用户那获取字符串

--- 第5章/listing_5-1.py
+++ 第5章/listing_5-1.py
@@ -1,10 +1,5 @@
-# Listing_5-1.py
-# Copyright Warren & Carter Sande, 2013
-# Released under MIT license   http://www.opensource.org/licenses/mit-license.php
-# Version $version  ----------------------------
+# 从用户那获取字符串
 
-# Getting a string from the user
-
-print "Enter your name: "
+print "输入你的名字: "
 somebody = raw_input()
-print "Hi", somebody, "how are you today?"
+print "Hi", somebody, "今天你感觉如何？"
