73ab06f0eba1d28e7879390646e86550b492ae5f
bingzgxsm <admin@gxsmjx.com>
2020-06-18 15:14:16 +0800

    update

--- 第5章/listing_5-4.py
+++ 第5章/listing_5-4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # 获取网页上的内容
 
-import urllib2
+import urllib2 #导入urllib2网络爬虫库
 # file = urllib2.urlopen('http://helloworldbook.com/data/message.txt')
 file = urllib2.urlopen('https://www.qq.com')
 message = file.read() 
