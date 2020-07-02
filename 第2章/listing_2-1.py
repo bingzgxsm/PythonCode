47a4dad4ba25468b986fdcc6f95898c0970e4908
bingzgxsm <admin@gxsmjx.com>
2020-07-02 09:10:52 +0800

    第二章更新

---
+++ 第2章/listing_2-1.py
@@ -0,0 +1,14 @@
+#定义变量First等于5、Second等于3
+First = 5
+Second = 3
+#直接显示两者相加
+print First + Second
+#定义Third来存储两者相加的值
+Third = First + Second
+#显示Third的值
+print Third
+#字符串也可以定义相加
+MyTeacher = "Mrs. Goodyear"
+YourTeacher = MyTeacher
+print MyTeacher
+print YourTeacher
